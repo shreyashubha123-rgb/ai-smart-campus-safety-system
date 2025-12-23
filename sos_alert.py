a35fa9a7deb6d5985ccbf4815ee8bf51903e32c3
shreyashubha123-rgb <shreyashubha123@gmail.com>
2025-12-24 00:10:11 +0530

    Add send_sos_alert function to send SOS alerts
    This function simulates sending an SOS alert to campus security with a specified location.

---
+++ sos_alert.py
@@ -0,0 +1,6 @@
+def send_sos_alert(location):
+    """
+    Smart SOS alert logic.
+    This simulates sending an alert to campus security.
+    """
+    return f"SOS alert sent to campus security at location: {location}"
